bd9b1e1d558aec9446df270103f69e8a2bc21f2d
ChathraNavoda <chathra96@gmail.com>
2025-05-29 17:04:40 +0530

    sign in with phone ui

---
+++ lib/presentation/screens/auth/phone_signin_screen.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+
+class PhoneSigninScreen extends StatefulWidget {
+  const PhoneSigninScreen({super.key});
+
+  @override
+  State<PhoneSigninScreen> createState() => _PhoneSigninScreenState();
+}
+
+class _PhoneSigninScreenState extends State<PhoneSigninScreen> {
+  TextEditingController phoneController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        phoneDialogBox(context);
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Container(
+          alignment: Alignment.center,
+          padding: EdgeInsets.symmetric(vertical: 14),
+          decoration: ShapeDecoration(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30),
+              side: const BorderSide(
+                color: Color.fromARGB(122, 0, 0, 0),
+              ),
+            ),
+            color: Colors.white,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              const Text(
+                'Continue with Phone',
+                style: TextStyle(
+                  color: Color.fromARGB(125, 0, 0, 0),
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              Icon(
+                Icons.phone,
+                color: Color(0xFF4ECDC4),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void phoneDialogBox(BuildContext context) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return Dialog(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              padding: EdgeInsets.all(20),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Phone Authentication',
+                        style: TextStyle(
+                          fontWeight: FontWeight.normal,
+                          fontSize: 18,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        icon: Icon(Icons.close),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  TextField(
+                    controller: phoneController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Enter your phone number',
+                      hintText: 'Eg: +94 780 4590',
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xFF4ECDC4),
+                    ),
+                    onPressed: () async {
+                      // await auth
+                      //     .sendPasswordResetEmail(email: emailController.text)
+                      //     .then((value) {
+                      //   showSnackBar(context,
+                      //       'We have sent you the reset password link to your email.');
+                      // }).onError((error, stackTrace) {
+                      //   showSnackBar(context, error.toString());
+                      // });
+                      // Navigator.pop(context);
+                      // emailController.clear();
+                    },
+                    child: Text(
+                      'Send Code',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                        color: Color.fromARGB(255, 255, 255, 255),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          );
+        });
+  }
+}
